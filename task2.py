370051fef54ff9c47fc0badcd76dff4d16b3c3c6
ICHpoken <misha_p2003@mail.ru>
2022-10-13 13:44:29 +0300

    com6

--- task2.py
+++
@@ -1,6 +0,0 @@
-def warn_the_sheep(queue):
-    if queue[::-1].index('wolf') == 0:
-        return 'Pls go away and stop eating my sheep'
-    else:
-        index_wolf = str(queue.index('wolf'))
-        return 'Oi! Sheep number ' + index_wolf + '! You are about to be eaten by a wolf!'
